b57f93f96dbd316dc795ad1cce9a71682040027f
Ian Kropp <iankropp@Ians-MacBook-Pro-2.local>
2016-11-10 22:01:42 -0500

    Finished a basic quick sort routine

--- sorting/quickSort/QuickSort.java
+++ sorting/quickSort/QuickSort.java
@@ -21,6 +21,36 @@ class QuickSort {
 
   }
 
+  public static ArrayList<Integer> quickSort(ArrayList<Integer> list){
+    
+    if(list.size() == 0)
+      return list;
+
+    ArrayList<Integer> lessThanEqualTo = new ArrayList<Integer>(0);
+    ArrayList<Integer> greaterThan = new ArrayList<Integer>(0);
+
+    int pivot = list.remove(0);
+    int originalListSize = list.size();
+
+    for(int i = 0; i < originalListSize; i++){
+   
+      if(list.get(0) > pivot){
+        greaterThan.add(list.remove(0));
+      } else {
+        lessThanEqualTo.add(list.remove(0));
+      }
+
+    }
+
+    lessThanEqualTo = quickSort(lessThanEqualTo);
+    greaterThan = quickSort(greaterThan);
+
+    lessThanEqualTo.add(pivot);
+    lessThanEqualTo.addAll(greaterThan);
+    return lessThanEqualTo;
+
+  }
+
   public static void main(String[] argv){
 
     ArrayList<Integer> list = new ArrayList<Integer>();
@@ -29,6 +59,19 @@ class QuickSort {
       System.err.println("Unable to parse array. Exiting."); 
       System.exit(1);
     }
+
+    System.out.println("Before:"); 
+
+    System.out.println(list.toString()); 
+    System.out.println("---"); 
+
+    list = quickSort(list);
+
+    System.out.println(""); 
+    System.out.println("---"); 
+    System.out.println("After:"); 
+    System.out.println(list.toString()); 
+
 
   }
 
