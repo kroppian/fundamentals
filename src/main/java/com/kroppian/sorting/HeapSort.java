258a92f1f2cc8e8244369e675290520e190185bb
Ian Kropp <iankropp@gmail.com>
2016-11-16 19:50:54 -0500

    Have some skeleton code up

--- src/main/java/com/kroppian/sorting/HeapSort.java
+++ src/main/java/com/kroppian/sorting/HeapSort.java
@@ -4,8 +4,57 @@ import com.kroppian.utils.SortingUtils;
 
 class HeapSort {
 
+  public static void sort(int arr[])
+  {
 
-  private static void swap(int arr[], int val1dex, int val2dex){
+    // Heapify from the bottom up
+    // for 
+      heapify(arr);
+
+    // for each item (i) in arr
+    for(int i = 0; i < arr.length; i++)
+    {
+
+    
+    }  
+
+  }
+
+  /*
+   * Assumes that all child nodes are already heapified
+   */
+  private static void heapify(int arr[])
+  {
+
+    // if the child is larger than the parent
+    
+      // Swap  child and parent
+
+      // Call heapify on child
+
+
+  }
+
+  private static int getParent(int index){
+  
+    if(index % 2 == 0)
+      parent = (index - 2) / 2;
+    else 
+      parent = (index - 1) / 2;
+
+    return parent; 
+
+  }
+
+  public static void siftDown(int arr[])
+  {
+   
+    
+
+  }
+
+  private static void swap(int arr[], int val1dex, int val2dex)
+  {
 
     int value1 = arr[val1dex];
     arr[val1dex] = arr[val2dex];
@@ -15,7 +64,8 @@ class HeapSort {
 
   }
 
-  public static void main(String argv[]){
+  public static void main(String argv[])
+  {
  
     int[] list;
 
